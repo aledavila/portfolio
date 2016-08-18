2d3faad2c9cfcb1484278bcf6b846957c77d271c
Alessandra Davila <alex.davila@utexas.edu>
2016-08-18 10:16:15 -0500

    initial commit

---
+++ public/js/app.js
@@ -0,0 +1,24 @@
+(function() {
+
+  var app = angular.module('Portfolio', ['ui.router']);
+
+  // configure different application states
+  app.config(function($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/');
+
+    $stateProvider
+      .state('home', {
+        url: '/',
+        templateUrl: '/templates/_homeView.html'
+      }).state('about', {
+        url: '/about',
+        templateUrl: '/templates/_aboutView.html'
+      }).state('projects', {
+        url: '/projects',
+        templateUrl: '/templates/_projectsView.html'
+      }).state('contact', {
+        url: '/contact',
+        templateUrl: '/templates/_contactView.html'
+      });
+  });
+})();
---
+++ public/js/main.js
@@ -0,0 +1,54 @@
+// TOGGLE MENU
+
+// jQuery
+// $(document).ready(function(){
+
+//   // $('.ui.sidebar').sidebar('attach events', '.toc.item');
+
+//   $('.exit').on('click', function() {
+//     setTimeout(function() {
+//       $('#home').fadeIn('slow');
+//     }, 500);
+//   });
+
+//   $('.home').on('click', function() {
+//     setTimeout(function() {
+//       $('#home').fadeIn('slow');
+//     }, 500);
+//   });
+
+//   $('.story').on('click', function() {
+//     if($(window).width() <= 767){
+//       $('.exit').show();
+//     }
+
+//     setTimeout(function() {
+//       $('#about').fadeIn('slow');
+//     }, 500);
+//   });
+
+//   $('#tech').on('click', function() {
+//     $('#techlist').toggle();
+//   });
+
+//   $('.projects').on('click', function() {
+//     if($(window).width() <= 767){
+//       $('.exit').show();
+//     }
+
+//     setTimeout(function() {
+//       $('#projects').fadeIn('slow');
+//     }, 500);
+//   });
+
+//   $('.contact').on('click', function() {
+//     if($(window).width() <= 767){
+//       $('.exit').show();
+//     }
+
+//     setTimeout(function() {
+//       $('#contact').fadeIn('slow');
+//     }, 500);
+//   });
+
+// });
