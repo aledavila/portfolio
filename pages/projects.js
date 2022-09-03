3b6654825989d99e8ff672ed94f4c46002fa560c
Alessandra Davila <aech@Alessandras-MBP.attlocal.net>
2022-09-02 22:23:50 -0500

    rename

--- pages/projects.js
+++ pages/projects.js
@@ -1,4 +1,4 @@
-import Layout from "../src/components/Layout/layout";
+import Layout from "../src/components/Layout/Layout";
 import Projects from "../src/components/Projects/Projects";
 
 export default function ProjectsPage() {
