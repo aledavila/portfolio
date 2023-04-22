1eaad94f4c0977d42fb584ac0427d7ed8295746e
Alessandra Davila <adavila91@gmail.com>
2023-04-22 14:59:15 -0500

    update dark mode to a button

--- src/components/darkMode/index.js
+++ src/components/darkMode/index.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react'
-import 'react-toggle/style.css'
-import Toggle from 'react-toggle'
+import 'react-toggle/style.css';
+import { FiMoon, FiSun } from 'react-icons/fi';
 
 function DarkModeToggle() {
   const [isDark, setIsDark] = useState(false)
@@ -19,8 +19,9 @@ function DarkModeToggle() {
   return (
     <div className="fixed top-4 right-4">
       <button
+        className='nav--link md:mr-4 md:px-2 md:rounded'
         type="button"
-        aria-label="Toggle dark mode"
+        aria-label={isDark ? 'Disable dark mode' : 'Enable dark mode'}
         onClick={toggleTheme}
         onKeyDown={(e) => {
           if (e.key === 'Enter' || e.key === ' ') {
@@ -29,44 +30,15 @@ function DarkModeToggle() {
         }}
         tabIndex="0"
       >
-        <span className="sr-only">
+        {/* <span className="sr-only">
           {isDark ? 'Disable dark mode' : 'Enable dark mode'}
         </span>
-        <span className="text-lg">
-          {isDark ? '🌙' : '🌞'}
-        </span>
+         */}
+        {isDark ? <FiMoon aria-hidden='true' /> : <FiSun aria-hidden='true' />}
       </button>
     </div>
   )
 }
-
-// function DarkModeToggle() {
-//   const [isDark, setIsDark] = useState(false)
-
-//   useEffect(() => {
-//     setIsDark(localStorage.getItem('theme') === 'dark')
-//   }, [])
-
-//   const toggleTheme = () => {
-//     const newTheme = isDark ? 'light' : 'dark'
-//     localStorage.setItem('theme', newTheme)
-//     setIsDark(!isDark)
-//     document.documentElement.classList.toggle('dark')
-//   }
-
-//   return (
-//     <div className="absolute bottom-4 right-4">
-//       <Toggle
-//         checked={isDark}
-//         icons={{
-//           checked: <span className="text-md">🌙</span>,
-//           unchecked: <span className="text-md">🌞</span>
-//         }}
-//         onChange={toggleTheme}
-//       />
-//     </div>
-//   )
-// }
 
 export default DarkModeToggle
 
